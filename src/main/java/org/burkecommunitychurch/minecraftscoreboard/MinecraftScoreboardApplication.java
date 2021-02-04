a51a5158537b00c56aa8dd0c935833c173bc504f
danieltmc <me@danieltmc.com>
2021-02-04 01:01:50 -0500

    Initial commit

---
+++ src/main/java/org/burkecommunitychurch/minecraftscoreboard/MinecraftScoreboardApplication.java
@@ -0,0 +1,13 @@
+package org.burkecommunitychurch.minecraftscoreboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinecraftScoreboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinecraftScoreboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/burkecommunitychurch/minecraftscoreboard/MinecraftScoreboardApplicationTests.java
@@ -0,0 +1,13 @@
+package org.burkecommunitychurch.minecraftscoreboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinecraftScoreboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
