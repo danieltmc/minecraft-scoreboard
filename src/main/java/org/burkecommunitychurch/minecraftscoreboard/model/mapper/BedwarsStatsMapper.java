22e7bf466dfcacfc1cb140834784d6424be3e9ee
danieltmc <me@danieltmc.com>
2021-02-10 12:50:38 -0500

    Fix tDto -> toDto

--- src/main/java/org/burkecommunitychurch/minecraftscoreboard/model/mapper/BedwarsStatsMapper.java
+++ src/main/java/org/burkecommunitychurch/minecraftscoreboard/model/mapper/BedwarsStatsMapper.java
@@ -4,7 +4,7 @@ import org.burkecommunitychurch.minecraftscoreboard.model.dto.BedwarsStatsDTO;
 import org.burkecommunitychurch.minecraftscoreboard.model.entity.BedwarsStats;
 
 public abstract class BedwarsStatsMapper {
-    public BedwarsStatsDTO tDto(BedwarsStats bedwarsStats) {
+    public BedwarsStatsDTO toDto(BedwarsStats bedwarsStats) {
         BedwarsStatsDTO dto = new BedwarsStatsDTO();
         dto.setPlayerUuid(bedwarsStats.getPlayerUuid());
         dto.setKey(bedwarsStats.getKey());
