9de8b8242880624734ee3c5616538890dbc0866f
danieltmc <me@danieltmc.com>
2021-02-10 23:14:24 -0500

    Disable package scanning

--- src/main/java/org/burkecommunitychurch/minecraftscoreboard/MinecraftScoreboardApplication.java
+++ src/main/java/org/burkecommunitychurch/minecraftscoreboard/MinecraftScoreboardApplication.java
@@ -3,7 +3,7 @@ package org.burkecommunitychurch.minecraftscoreboard;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"org.burkecommunitychurch.minecraft-scoreboard"})
+//@SpringBootApplication(scanBasePackages = {"org.burkecommunitychurch.minecraft-scoreboard"})
 public class MinecraftScoreboardApplication {
 
 	public static void main(String[] args) {
