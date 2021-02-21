9286302ea9c3bffc36244d945232eff813b9aad6
danieltmc <me@danieltmc.com>
2021-02-21 10:51:14 -0500

    Add ApiInfo and dev contact info

--- src/main/java/org/burkecommunitychurch/minecraftscoreboard/config/SpringFoxConfig.java
+++ src/main/java/org/burkecommunitychurch/minecraftscoreboard/config/SpringFoxConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Import;
 import springfox.documentation.RequestHandler;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -24,5 +26,19 @@ public class SpringFoxConfig {
                 .apis((Predicate<RequestHandler>) RequestHandlerSelectors.any())
                 .paths((Predicate<String>) PathSelectors.any())
                 .build();
+    }
+
+    private ApiInfo apiInfo() {
+        ApiInfo apiInfo = new ApiInfo(
+            "BCC Minecraft Scoreboard",
+            "List scores for MCBCC minigames.",
+            "1.0.0",
+            "N/A",
+            new Contact("Daniel McCloskey", "danieltmc.com", "me@danieltmc.com"),
+            "N/A",
+            "N/A",
+            null
+        );
+        return apiInfo;
     }
 }
