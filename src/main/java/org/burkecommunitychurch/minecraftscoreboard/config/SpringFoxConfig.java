c60f36f169b7b3e9879d25ecceb722f6842f52ab
danieltmc <me@danieltmc.com>
2021-02-21 10:38:41 -0500

    Import SpringfoxWebConfiguration

--- src/main/java/org/burkecommunitychurch/minecraftscoreboard/config/SpringFoxConfig.java
+++ src/main/java/org/burkecommunitychurch/minecraftscoreboard/config/SpringFoxConfig.java
@@ -4,16 +4,19 @@ import com.google.common.base.Predicate;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 import springfox.documentation.RequestHandler;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableSwagger2
 @Configuration
+@Import(SpringDataRestConfiguration.class)
 public class SpringFoxConfig {
     @Bean
     public Docket api() {
