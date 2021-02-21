92c2fa73620aae34938706d67b8246dd089d2e48
danieltmc <me@danieltmc.com>
2021-02-21 16:19:38 -0500

    Simplify Swagger Docket

--- src/main/java/org/burkecommunitychurch/minecraftscoreboard/config/SwaggerConfig.java
+++ src/main/java/org/burkecommunitychurch/minecraftscoreboard/config/SwaggerConfig.java
@@ -1,18 +1,11 @@
 package org.burkecommunitychurch.minecraftscoreboard.config;
 
-import com.google.common.base.Predicate;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 
-import springfox.documentation.RequestHandler;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -21,10 +14,11 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class SwaggerConfig {
     @Bean
     public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2).select()
-                .apis((Predicate<RequestHandler>) RequestHandlerSelectors.any())
-                .paths((Predicate<String>) PathSelectors.any())
-                .build();
+        // return new Docket(DocumentationType.SWAGGER_2).select()
+        //         .apis((Predicate<RequestHandler>) RequestHandlerSelectors.any())
+        //         .paths((Predicate<String>) PathSelectors.any())
+        //         .build();
+        return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo());
     }
 
     private ApiInfo apiInfo() {
