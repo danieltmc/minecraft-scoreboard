edabc01f4a4bedfc95a3f1f22952c4668dc43744
danieltmc <me@danieltmc.com>
2021-02-21 19:14:43 -0500

    Escape colon character in SQL nativeQuery and remove colon from BedwarsStatsKeyEnum

--- src/main/java/org/burkecommunitychurch/minecraftscoreboard/model/constant/BedwarsStatsKeyEnum.java
+++ src/main/java/org/burkecommunitychurch/minecraftscoreboard/model/constant/BedwarsStatsKeyEnum.java
@@ -1,18 +1,18 @@
 package org.burkecommunitychurch.minecraftscoreboard.model.constant;
 
 public enum BedwarsStatsKeyEnum {
-    BEDS_DESTROYED("bedwars:beds_destroyed"),
-    CACHE_RANK("bedwars:cache_rank"),
-    DEATHS("bedwars:deaths"),
-    FINAL_KILLS("bedwars:final_kills"),
-    KILL_DEATH_RATIO("bedwars:kd"),
-    KILLS("bedwars:kills"),
-    LOSSES("bedwars:loses"),
-    PLAY_TIME("bedwars:play_time"),
-    RANK("bedwars:rank"),
-    ROUNDS_PLAYED("bedwars:rounds_played"),
-    WINS("bedwars:wins"),
-    WIN_LOSS("bedwars:wl");
+    BEDS_DESTROYED("beds_destroyed"),
+    CACHE_RANK("cache_rank"),
+    DEATHS("deaths"),
+    FINAL_KILLS("final_kills"),
+    KILL_DEATH_RATIO("kd"),
+    KILLS("kills"),
+    LOSSES("loses"),
+    PLAY_TIME("play_time"),
+    RANK("rank"),
+    ROUNDS_PLAYED("rounds_played"),
+    WINS("wins"),
+    WIN_LOSS("wl");
 
     private final String sqlKey;
 
