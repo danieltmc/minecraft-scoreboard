fd7c5b115e99d0a1e31a5bc3e3f7c1c38ecb893c
danieltmc <me@danieltmc.com>
2021-02-22 22:35:25 -0500

    Map userName from Mojang API

--- src/main/java/org/burkecommunitychurch/minecraftscoreboard/model/mapper/BedwarsAchievementsMapper.java
+++ src/main/java/org/burkecommunitychurch/minecraftscoreboard/model/mapper/BedwarsAchievementsMapper.java
@@ -1,11 +1,18 @@
 package org.burkecommunitychurch.minecraftscoreboard.model.mapper;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import org.burkecommunitychurch.minecraftscoreboard.model.dto.BedwarsAchievementsDTO;
 import org.burkecommunitychurch.minecraftscoreboard.model.entity.BedwarsAchievements;
+import org.burkecommunitychurch.minecraftscoreboard.model.entity.MojangPlayer;
 import org.mapstruct.Mapper;
 
 @Mapper
@@ -13,6 +20,18 @@ public abstract class BedwarsAchievementsMapper {
     public BedwarsAchievementsDTO toDto(BedwarsAchievements bedwarsAchievements) {
         BedwarsAchievementsDTO dto = new BedwarsAchievementsDTO();
         dto.setPlayerUuid(bedwarsAchievements.getPlayerUuid());
+
+        // Fetch and assign userName
+        ObjectMapper objectMapper = new ObjectMapper();
+        MojangPlayer mojangPlayer = new MojangPlayer();
+        try {
+            mojangPlayer = objectMapper.readValue(new URL("https://sessionserver.mojang.com/session/minecraft/profile/"
+                    + bedwarsAchievements.getPlayerUuid()), MojangPlayer.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        dto.setUserName(mojangPlayer.getName());
+
         dto.setId(bedwarsAchievements.getId());
         dto.setEarnDate(bedwarsAchievements.getEarnDate());
         return dto;
