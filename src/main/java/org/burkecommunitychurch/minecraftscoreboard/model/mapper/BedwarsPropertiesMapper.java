91693be95a454beca51254b8c82b8c64d00785d2
danieltmc <me@danieltmc.com>
2021-02-22 22:41:02 -0500

    Map userName in BedwarsPropertiesDTO

--- src/main/java/org/burkecommunitychurch/minecraftscoreboard/model/mapper/BedwarsPropertiesMapper.java
+++ src/main/java/org/burkecommunitychurch/minecraftscoreboard/model/mapper/BedwarsPropertiesMapper.java
@@ -1,11 +1,16 @@
 package org.burkecommunitychurch.minecraftscoreboard.model.mapper;
 
+import java.io.IOException;
+import java.net.URL;
 import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import org.burkecommunitychurch.minecraftscoreboard.model.dto.BedwarsPropertiesDTO;
 import org.burkecommunitychurch.minecraftscoreboard.model.entity.BedwarsProperties;
+import org.burkecommunitychurch.minecraftscoreboard.model.entity.MojangPlayer;
 import org.mapstruct.Mapper;
 
 @Mapper
@@ -13,6 +18,18 @@ public abstract class BedwarsPropertiesMapper {
     public BedwarsPropertiesDTO toDto(BedwarsProperties bedwarsProperties) {
         BedwarsPropertiesDTO dto = new BedwarsPropertiesDTO();
         dto.setPlayerUuid(bedwarsProperties.getPlayerUuid());
+
+        // Fetch and assign userName
+        ObjectMapper objectMapper = new ObjectMapper();
+        MojangPlayer mojangPlayer = new MojangPlayer();
+        try {
+            mojangPlayer = objectMapper.readValue(new URL("https://sessionserver.mojang.com/session/minecraft/profile/"
+                    + bedwarsProperties.getPlayerUuid()), MojangPlayer.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        dto.setUserName(mojangPlayer.getName());
+
         dto.setId(bedwarsProperties.getId());
         dto.setValue(bedwarsProperties.getValue());
         return dto;
