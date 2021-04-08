7da5613f86d3942c60a2b8eb0810834f8565ecb7
Daniel McCloskey <me@danieltmc.com>
2021-04-08 14:10:23 -0400

    Add service and transactional annotations to TheWallsService

--- src/main/java/org/burkecommunitychurch/minecraftscoreboard/service/TheWallsServiceImpl.java
+++ src/main/java/org/burkecommunitychurch/minecraftscoreboard/service/TheWallsServiceImpl.java
@@ -2,11 +2,16 @@ package org.burkecommunitychurch.minecraftscoreboard.service;
 
 import java.util.List;
 
+import javax.transaction.Transactional;
+
 import org.burkecommunitychurch.minecraftscoreboard.model.dto.TheWallsDTO;
 import org.burkecommunitychurch.minecraftscoreboard.model.mapper.TheWallsMapper;
 import org.burkecommunitychurch.minecraftscoreboard.repository.TheWallsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
+@Transactional
 public class TheWallsServiceImpl implements TheWallsService {
     @Autowired
     private TheWallsRepository repo;
